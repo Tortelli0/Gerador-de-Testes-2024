8e73d0401cea3fe6b346ef23c26c6a83572c5769
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-06-13 21:14:04 -0300

    Criado layout da tela de cadastro de testes

---
+++ Gerador-de-Testes-2024/ModuloTeste/TelaTesteForm.cs
@@ -0,0 +1,13 @@
+﻿using GeradorDeTestes2024.Compartilhado;
+
+namespace GeradorDeTestes2024.ModuloTeste
+{
+    public partial class TelaTesteForm : Form
+    {
+        public TelaTesteForm()
+        {
+            InitializeComponent();
+            this.ConfigurarDialog();
+        }
+    }
+}
--- Gerador-de-Testes-2024/ModuloTeste/Teste.cs
+++ Gerador-de-Testes-2024/ModuloTeste/Teste.cs
@@ -1,6 +1,18 @@
-﻿namespace GeradorDeTestes2024.ModuloTeste
+﻿using GeradorDeTestes.ConsoleApp.Compartilhado;
+
+namespace GeradorDeTestes2024.ModuloTeste
 {
-    public class Teste
+    public class Teste : EntidadeBase
     {
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<string> Validar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
